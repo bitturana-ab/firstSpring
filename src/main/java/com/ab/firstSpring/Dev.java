75593fa1f491becdf518d6da4b8711cbba98fee3
bitturana-ab <b2ra830@gmail.com>
2025-07-16 23:51:19 +0530

    autowired of object

--- src/main/java/com/ab/firstSpring/Dev.java
+++ src/main/java/com/ab/firstSpring/Dev.java
@@ -1,10 +1,15 @@
 package com.ab.firstSpring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Dev {
+
+    @Autowired
+    private Laptop laptop;
     public void build(){
-        System.out.println("working on spring project");
+        laptop.compile();
+        System.out.println("working on spring project.");
     }
 }
