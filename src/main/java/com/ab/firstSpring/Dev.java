93adda8f81a78582dda267fe1c881222a742ed91
bitturana-ab <b2ra830@gmail.com>
2025-07-16 23:43:28 +0530

    manage and create object by spring

--- src/main/java/com/ab/firstSpring/Dev.java
+++ src/main/java/com/ab/firstSpring/Dev.java
@@ -1,5 +1,8 @@
 package com.ab.firstSpring;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Dev {
     public void build(){
         System.out.println("working on spring project");
--- src/main/java/com/ab/firstSpring/FirstSpringApplication.java
+++ src/main/java/com/ab/firstSpring/FirstSpringApplication.java
@@ -2,14 +2,15 @@ package com.ab.firstSpring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class FirstSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FirstSpringApplication.class, args);
-		// create object of Dev class manually
-		Dev dev = new Dev();
+		ApplicationContext context = SpringApplication.run(FirstSpringApplication.class, args);
+		// manage object by spring
+		Dev dev = context.getBean(Dev.class);
 		dev.build();
 	}
 
